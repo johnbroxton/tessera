a0f8e972359fa3f9a936cd2729c0c28af9dbb7c5
John Broxton <johnbroxton@gmail.com>
2015-09-29 15:04:37 -0700

    Initial commit of Tessera

---
+++ tessera/tessera/local_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tessera',
+        'USER': 'jbroxton',
+        'PASSWORD': 'sardonis99',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+} 
+
